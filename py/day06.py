f011d9b352cc75dfcdd326f62aba666b25122554
Jan Christoph Terasa <christoph@kohlio.de>
2020-12-20 08:10:04 +0100

    Solved day 06.

---
+++ py/day06.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+def part1(lines):
+    group = set()
+    answers = 0
+    for line in lines:
+        if line:
+            group |= set(line)
+        else:
+            answers += len(group)
+            group = set()
+
+    return answers
+
+
+def part2(lines):
+    group = set(chr(x) for x in range(ord('a'), ord('z') + 1))
+    answers = 0
+    for line in lines:
+        if line:
+            group &= set(line)
+        else:
+            answers += len(group)
+            group = set(chr(x) for x in range(ord('a'), ord('z') + 1))
+
+    return answers
+
+
+with open("../input/day06.input") as f:
+    lines = [x.strip() for x in f.readlines()]
+    print("part1:", part1(lines))
+    print("part2:", part2(lines))
